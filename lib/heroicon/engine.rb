2e411ed59b733c1b9565dd037dbf19d4c9f93220
zhongsheng <nily.zhong@gmail.com>
2020-12-28 11:13:29 +0800

    add hook

--- lib/heroicon/engine.rb
+++ lib/heroicon/engine.rb
@@ -3,5 +3,11 @@
 module Heroicon
   class Engine < ::Rails::Engine
     isolate_namespace Heroicon
+
+    initializer "heroicon.helper" do
+      ActiveSupport.on_load(:action_controller_base) do
+        helper Heroicon::Engine.helpers
+      end
+    end
   end
 end
