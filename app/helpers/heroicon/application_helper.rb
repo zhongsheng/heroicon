728c6efe4dc9353986eb3c6fa4dc571290ac01fc
zhongsheng <nily.zhong@gmail.com>
2020-12-28 11:14:22 +0800

    use **options

--- app/helpers/heroicon/application_helper.rb
+++ app/helpers/heroicon/application_helper.rb
@@ -2,7 +2,7 @@
 
 module Heroicon
   module ApplicationHelper
-    def heroicon(name, variant: Heroicon.configuration.variant, options: {})
+    def heroicon(name, variant: Heroicon.configuration.variant, **options)
       raw Heroicon::Icon.render(name: name, variant: variant, options: options)
     end
   end
